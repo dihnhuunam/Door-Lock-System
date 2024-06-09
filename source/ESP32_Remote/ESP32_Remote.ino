c97f42d5ad0fe74f72635f7a9ab7d8c84c9975f2
dihnhuunam <dihnhuunam@gmail.com>
2024-06-09 23:52:06 +0700

    [REFACTOR]: delete old folders

--- source/ESP32_Remote/ESP32_Remote.ino
+++ source/ESP32_Remote/ESP32_Remote.ino
@@ -1,164 +1,53 @@
 #include <WiFi.h>
-#include <WebServer.h>
-#include <FS.h>
-#include <SPIFFS.h>
-#include <SoftwareSerial.h>
+#include <HTTPClient.h>
 
-// WiFi credentials
-const char *ssid = "NHUNG1975";
-const char *password = "0971947652";
+const char* ssid = "NHUNG1975";
+const char* password = "0971947652";
 
-// Unlock password
-const char *unlockPassword = "1234";
+const char* serverUrl = "http://localhost:8000/";
 
-// Create a web server on port 80
-WebServer server(80);
-
-// Define SoftwareSerial pins for ESP32
-#define RX_PIN 16 // Connect GPIO 16 of ESP32 to D3 of Arduino Uno
-#define TX_PIN 17 // Connect GPIO 17 of ESP32 to D2 of Arduino Uno
-
-//  SoftwareSerial object for ESP32 to communicate with Arduino Uno
-SoftwareSerial espSerial(RX_PIN, TX_PIN);
-
-// Connect to WiFi
-void connectToWiFi()
-{
+void setup() {
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi...");
+    Serial.print(".");
   }
   Serial.println("Connected to WiFi");
-  // Print the IP address
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
-// Initialize SPIFFS
-void initializeSPIFFS()
-{
-  if (!SPIFFS.begin(true))
-  {
-    Serial.println("An error has occurred while mounting SPIFFS");
-    return;
-  }
-  Serial.println("SPIFFS mounted successfully");
-}
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
 
-// Handle root URL (serve index.html)
-void handleRoot()
-{
-  File file = SPIFFS.open("/index.html", "r");
-  if (!file)
-  {
-    server.send(500, "text/plain", "Failed to open index.html");
-    return;
-  }
-  String html = file.readString();
-  server.send(200, "text/html", html);
-  file.close();
-}
-// Handle CSS requests (serve styles.css)
-void handleCSS()
-{
-  File file = SPIFFS.open("/styles.css", "r");
-  if (!file)
-  {
-    server.send(500, "text/plain", "Failed to open styles.css");
-    return;
-  }
-  String css = file.readString();
-  server.send(200, "text/css", css);
-  file.close();
-}
-// Handle unlock request
-void handleUnlock()
-{
-  if (server.hasArg("password"))
-  {
-    String password = server.arg("password");
-    if (password.equals(unlockPassword))
-    {
-      // Send unlock command to Arduino
-      espSerial.println("UNLOCK");
-      server.send(200, "text/plain", "Door unlocked successfully!");
-    }
-    else
-    {
-      server.send(401, "text/plain", "Incorrect password. Please try again.");
-    }
-  }
-  else
-  {
-    server.send(400, "text/plain", "Bad Request");
-  }
-}
-// Start the WebServer
-void startWebServer()
-{
-  // Start web server
-  server.on("/", handleRoot);
-  server.on("/styles.css", handleCSS);
-  server.on("/unlock", HTTP_POST, handleUnlock);
-  server.begin();
-  Serial.println("HTTP server started");
-}
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
 
-// Check connection with Arduino Uno
-bool checkArduinConnection()
-{
-  espSerial.println("CHECK_CONNECTION");
-  int startTime = millis();
-  while (millis() - startTime < 2000)
-  { // wait for 2 seconds
-    if (espSerial.available())
-    {
-      String response = espSerial.readStringUntil('\n');
-      response.trim();
-      Serial.print("Response from Arduino: ");
+    // Replace with actual password input logic
+    String payload = "{\"password\":\"1234\"}";
+
+    int httpResponseCode = http.POST(payload);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
       Serial.println(response);
-      if (response == "CONNECTED SUCCESSFULLY")
-      {
-        return true;
+
+      if (response == "Password is correct, door unlocked") {
+        // Code to unlock the door (e.g., move servo motor)
+        Serial.println("Unlocking door...");
+      } else {
+        Serial.println("Access denied");
       }
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
     }
+
+    http.end();
   }
-  return false;
-}
 
-void checkWiFiConnection()
-{
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  delay(10000);
-}
-
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("ESP32 starting...");
-  espSerial.begin(9600); // SoftwareSerial at baudrate 9600
-
-  connectToWiFi();
-  initializeSPIFFS();
-  startWebServer();
-  delay(5000);
-
-  // Check connection with Arduino
-  if (checkArduinConnection())
-  {
-    Serial.println("Connection with Arduino successful");
-  }
-  else
-  {
-    Serial.println("Failed to connect to Arduino");
-  }
-}
-
-void loop()
-{
-  server.handleClient(); // Handle client requests
-  checkWiFiConnection();
+  delay(10000); // Delay for demonstration purposes
 }
--- source/Mobile_App/server/app/routes/passwordRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const passwordController = require("../controllers/passwordController");
-
-// Post request to check password
-router.post('/check_password', passwordController.checkPassword)
-
-module.exports = router;
