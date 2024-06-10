b54e8c8eaa7c42b388674671ba6fe27fdc24bb95
dihnhuunam <dihnhuunam@gmail.com>
2024-06-11 00:19:01 +0700

    [REFACTOR]: format code

--- source/Unlock_App/app/models/unlockModel.js
+++ source/Unlock_App/app/models/unlockModel.js
@@ -1,31 +1,41 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const unlockDoor = async (password) => {
-  const url = 'http://localhost:8000/unlock';
+  const url = "http://localhost:8000/unlock";
   try {
-    const response = await axios.post(url, `password=${encodeURIComponent(password)}`, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
+    const response = await axios.post(
+      url,
+      `password=${encodeURIComponent(password)}`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
 
     return response.status;
   } catch (error) {
-    throw new Error('Network error');
+    throw new Error("Network error");
   }
 };
 
 export const changeUserPassword = async (currentPassword, newPassword) => {
-  const url = 'http://localhost:8000/change-password';
+  const url = "http://localhost:8000/change-password";
   try {
-    const response = await axios.post(url, `currentPassword=${encodeURIComponent(currentPassword)}&newPassword=${encodeURIComponent(newPassword)}`, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
+    const response = await axios.post(
+      url,
+      `currentPassword=${encodeURIComponent(
+        currentPassword
+      )}&newPassword=${encodeURIComponent(newPassword)}`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
 
     return response.status;
   } catch (error) {
-    throw new Error('Network error');
+    throw new Error("Network error");
   }
 };
