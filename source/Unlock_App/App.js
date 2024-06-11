3bceb5a9e35ab7585368c2e8f890134a8348c9fd
dihnhuunam <dihnhuunam@gmail.com>
2024-06-11 21:33:33 +0700

    [ADD]: UI/UX completed

--- source/Unlock_App/App.js
+++ source/Unlock_App/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import UnlockScreen from './app/screens/UnlockScreen1';
+import UnlockScreen from './app/screens/UnlockScreen';
 import ChangePasswordScreen from './app/screens/ChangePasswordScreen';
+import AddPasswordScreen from './app/screens/AddPasswordScreen';
+import HomeScreen from './app/screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="UnlockScreen">
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Home' }} />
         <Stack.Screen name="UnlockScreen" component={UnlockScreen} options={{ title: 'Unlock Door' }} />
         <Stack.Screen name="ChangePasswordScreen" component={ChangePasswordScreen} options={{ title: 'Change Password' }} />
+        <Stack.Screen name="AddPasswordScreen" component={AddPasswordScreen} options={{ title: 'Add Password' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
